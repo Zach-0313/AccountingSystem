42957188fa3a729a2d4559960e3324d3459ab085
zachmalta <zach.malta@inveristraining.com>
2025-01-27 19:10:03 -0500

    Basic Login Screen

---
+++ applicationdomainproject/src/LoginScreen.tsx
@@ -0,0 +1,19 @@
+function LoginScreen() {
+    return (
+        <section>
+            <h1>Login Screen</h1>
+                <p>
+                    <p>
+                        <h4>Username</h4>
+                            <input type="text" name="name" />
+                    </p>
+                <h4>Password</h4>
+
+                <input type="text" name="name" />
+                </p>
+       </section>
+
+  );
+}
+
+export default LoginScreen;
