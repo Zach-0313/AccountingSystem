f67440adb1911ca46ada53fc60c812f45d8e4e68
Zach-0313 <72165465+Zach-0313@users.noreply.github.com>
2025-03-11 18:42:59 -0400

    Initial commit
    added a table to the database, added screen to view the table, added screen for detailed view

--- applicationdomainproject/src/App.tsx
+++ applicationdomainproject/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
 import LoginScreen from './LoginScreen.tsx'
+import AccountView from './AccountView.tsx'
 
 function App() {
 
   return (
     <>
       <div>
-        <LoginScreen />
+        <AccountView />
         </div>
     </>
   )
